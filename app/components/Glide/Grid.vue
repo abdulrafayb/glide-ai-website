7f97e2dfe44a3b7e1b63da48dd9fda7a8aaca9e1
abdulrafayb <abdulrafay200066@gmail.com>
2025-10-28 02:57:59 +0500

    working on hero section animations

---
+++ app/components/Glide/Grid.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const grid = [14, 30];
+</script>
+
+<template>
+  <svg
+    id="glide-grid"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 935 425"
+    class="absolute -left-2 -top-14 -z-10"
+    style="mask-image: linear-gradient(black, transparent)"
+  >
+    <g class="glide-grid-group">
+      <template v-for="i in grid[0]" :key="i">
+        <path
+          v-for="j in grid[1]"
+          :key="j"
+          fill="currentColor"
+          opacity=".2"
+          class="glide-grid-item"
+          :d="`M${(j - 1) * 32 + 5} ${(i - 1) * 32 + 10}l1.806,-2.951l-5,2.951l3.936,1.049l-0.742,-1.049`"
+        />
+      </template>
+    </g>
+  </svg>
+</template>
+
+<!-- d="M3.19355 2.95082L5 0L0 2.95082L3.93548 4L3.19355 2.95082Z" -->
--- app/slices/Hero/index.vue
+++ app/slices/Hero/index.vue
@@ -1,17 +1,85 @@
 <script setup lang="ts">
-import gsap from "gsap";
-import type { Content } from "@prismicio/client";
+import gsap from 'gsap';
+import type { Content } from '@prismicio/client';
 
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
 defineProps(
   getSliceComponentProps<Content.HeroSlice>([
-    "slice",
-    "index",
-    "slices",
-    "context",
+    'slice',
+    'index',
+    'slices',
+    'context',
   ])
 );
+
+onMounted(() => {
+  const preferReducedMotion = window.matchMedia(
+    '(prefers-reduced-motion: reduce)'
+  ).matches;
+
+  if (preferReducedMotion) {
+    // removing animations for accessibility
+    return;
+  }
+
+  const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+
+  tl.fromTo(
+    '.hero_heading',
+    { scale: 0.5, opacity: 0 },
+    { scale: 1, opacity: 1, duration: 1.4 }
+  );
+  tl.fromTo(
+    '.hero_body',
+    { y: 20, opacity: 0 },
+    { y: 0, opacity: 1, duration: 1.2 },
+    '-=0.6'
+  );
+  tl.fromTo(
+    '.hero_ctas',
+    { scale: 1.5, opacity: 0 },
+    { scale: 1, opacity: 1, duration: 1.3 },
+    '-=0.8'
+  );
+  tl.fromTo(
+    '.hero_image',
+    { y: 100, opacity: 0 },
+    { y: 0, opacity: 1, duration: 1.1 },
+    '+=0.3'
+  );
+  tl.fromTo(
+    '.hero_glow',
+    { scale: 0.5, opacity: 0 },
+    { scale: 1, opacity: 1, duration: 1.8 },
+    '+=1'
+  );
+
+  tl.to('.hero_glow_one', {
+    ease: 'power2.inOut',
+    repeat: -1,
+    repeatDelay: 0,
+    keyframes: [
+      { top: '0%', left: '33%', duration: 0 },
+      { top: '33%', left: '33%', duration: 2 },
+      { top: '33%', left: '0%', duration: 3 },
+      { top: '0%', left: '0%', duration: 2 },
+      { top: '0%', left: '33%', duration: 3 },
+    ],
+  });
+  tl.to('.hero_glow_two', {
+    ease: 'power2.inOut',
+    repeat: -1,
+    repeatDelay: 0,
+    keyframes: [
+      { top: '33%', left: '0%', duration: 0 },
+      { top: '0%', left: '0%', duration: 2 },
+      { top: '0%', left: '33%', duration: 3 },
+      { top: '33%', left: '33%', duration: 2 },
+      { top: '33%', left: '0%', duration: 3 },
+    ],
+  });
+});
 </script>
 
 <template>
@@ -20,17 +88,18 @@ defineProps(
     :data-slice-variation="slice.variation"
   >
     <div class="text-center relative">
+      <GlideGrid />
       <PrismicText
         :field="slice.primary.heading"
         wrapper="h1"
-        class="mx-auto max-w-3xl text-balance text-5xl font-medium"
+        class="hero_heading mx-auto max-w-3xl text-balance text-5xl font-medium"
       />
       <PrismicText
         :field="slice.primary.body"
         wrapper="p"
-        class="mx-auto mt-6 max-w-md text-balance text-gray-300"
+        class="hero_body mx-auto mt-6 max-w-md text-balance text-gray-300"
       />
-      <div class="flex flex-wrap gap-8 justify-center mt-8">
+      <div class="hero_ctas flex flex-wrap gap-8 justify-center mt-8">
         <PrismicLink
           v-for="cta in slice.primary.ctas"
           :key="cta.key"
@@ -38,14 +107,14 @@ defineProps(
           class="buttonLink"
         />
       </div>
-      <div class="glassContainer mt-16 w-fit">
+      <div class="hero_image glassContainer mt-16 w-fit">
         <div
-          class="absolute left-1/3 top-0 -z-10 h-2/3 w-2/3 bg-sky-700/50 blur-3xl md:blur-[120px] filter mix-blend-screen"
+          class="hero_glow hero_glow_one absolute left-1/3 top-0 -z-10 h-2/3 w-2/3 bg-sky-700/50 blur-3xl md:blur-[120px] filter mix-blend-screen"
         />
         <div
-          class="absolute left-0 top-1/3 -z-10 h-2/3 w-2/3 bg-teal-600/50 blur-3xl md:blur-[120px] filter mix-blend-screen"
+          class="hero_glow hero_glow_two absolute left-0 top-1/3 -z-10 h-2/3 w-2/3 bg-teal-600/50 blur-3xl md:blur-[120px] filter mix-blend-screen"
         />
-        <PrismicImage :field="slice.primary.image" />
+        <PrismicImage :field="slice.primary.image" class="rounded-lg" />
       </div>
     </div>
   </Bounded>
