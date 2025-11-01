007afeefbe0f0e013b0cb7ad2ece33c44fb227db
abdulrafayb <abdulrafay200066@gmail.com>
2025-11-02 02:37:45 +0500

    completed showcase

--- app/slices/Showcase/index.vue
+++ app/slices/Showcase/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import type { Content } from '@prismicio/client';
 
 // The array passed to `getSliceComponentProps` is purely optional.
@@ -11,6 +13,47 @@ defineProps(
     'context',
   ])
 );
+
+onMounted(() => {
+  const preferReducedMotion = window.matchMedia(
+    '(prefers-reduced-motion: reduce)'
+  ).matches;
+
+  if (preferReducedMotion) return;
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.fromTo(
+    '.showcase_heading',
+    { y: 100 },
+    {
+      y: 0,
+      ease: 'power2.inOut',
+      duration: 1,
+      scrollTrigger: {
+        trigger: '.showcase_heading',
+        start: 'top center',
+        toggleActions: 'play pause resume reverse',
+      },
+    }
+  );
+
+  gsap.fromTo(
+    '.showcase_glow',
+    { scale: 0.7, opacity: 0.1 },
+    {
+      scale: 1,
+      opacity: 1,
+      ease: 'power2.inOut',
+      duration: 1,
+      scrollTrigger: {
+        trigger: '.showcase_heading',
+        start: 'top center',
+        toggleActions: 'play pause resume reverse',
+      },
+    }
+  );
+});
 </script>
 
 <template>
@@ -18,14 +61,18 @@ defineProps(
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
+    <div
+      class="showcase_glow absolute -z-10 aspect-video w-full max-w-2xl rounded-full bg-sky-700 blur-[120px] filter mix-blend-screen"
+    />
     <PrismicRichText
       :field="slice.primary.heading"
       wrapper="header"
-      class="text-balance text-center text-5xl font-medium md:text-7xl"
+      class="showcase_heading text-balance text-center text-5xl font-medium md:text-7xl"
     />
     <article
       class="relative mt-16 grid items-center gap-8 rounded-xl border border-sky-50/20 bg-gradient-to-b from-gray-50/15 to-gray-50/5 p-8 backdrop-blur-sm lg:grid-cols-3 lg:gap-0 lg:py-12"
     >
+      <div class="gridBackground" />
       <div>
         <figure class="w-fit rounded-lg bg-sky-900 p-4 text-3xl">
           <Icon :name="slice.primary.icon" class="block" />
@@ -48,9 +95,22 @@ defineProps(
         :class="
           slice.variation === 'reversed'
             ? 'lg:order-1 lg:translate-x-[15%]'
-            : 'lg:order-1 lg:translate-x-[-15%]'
+            : 'lg:-order-1 lg:translate-x-[-15%]'
         "
       />
     </article>
   </Bounded>
 </template>
+
+<style scoped>
+.gridBackground {
+  background-image: url('/assets/grid-pattern.png');
+  position: absolute;
+  inset: 0;
+  background-repeat: repeat;
+  z-index: -1;
+  background-position: center;
+  opacity: 0.15;
+  mask-image: radial-gradient(circle at 60% 50%, black 10%, transparent 40%);
+}
+</style>
